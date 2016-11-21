31e049aa718d1e910b4e21e7707403833f264fc3
Chris Sullens <cls.seals@gmail.com>
2016-11-21 15:09:02 -0500

    Jenkins automatic commit - 'jenkins-Screeps - Sync GitHub Repo-23'

--- newCostMatrix.js
+++ newCostMatrix.js
@@ -3,7 +3,7 @@
  * module.exports.thing = 'a thing';
  *
  * You can import it from another modules like this:
- * var mod = require('newCostMatrix');
+ * var mod = require('CostMatrix');
  * mod.thing == 'a thing'; // true
  */
 
