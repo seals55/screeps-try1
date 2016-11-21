114f45a1b2cb3c6c0008b8356d6a34d900bb7bb1
Chris Sullens <cls.seals@gmail.com>
2016-11-21 07:48:02 -0500

    Jenkins automatic commit - 'jenkins-Screeps - Sync GitHub Repo-12'

--- backup.bat
+++ backup.bat
@@ -5,7 +5,7 @@ set BaseDir=C:\Users\csull\AppData\Local\Screeps
 set DestDir=D:\ScreepsBackup
 set ExtFilter=js
 set ZipFilter=gz
-set Zip=c:\program files\7-zip\7za.exe
+set Zip=c:\program files\7-zip\7z.exe
 
 REM Current date time in YYYY_MM_DD_hh_mm_ss format for timestamp on moved files
 set datetime=%date:~-4%_%date:~-7,2%_%date:~-10,2%_%time:~0,2%_%time:~3,2%_%time:~6,2%
