d78cd1c3c539a90e1a5d49664632e6ba7a86bbfc
Chris Sullens <cls.seals@gmail.com>
2016-11-21 23:37:02 -0500

    Jenkins automatic commit - 'jenkins-Screeps - Sync GitHub Repo-98'

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ module.exports.loop = function () {
     for (var name in Memory.creeps) {
         if (!Game.creeps[name]) {
             delete Memory.creeps[name];
-            console.log('Clearing non-existing creep memory:', name);
+            console.log('Clearing non-existing creep from memory:', name);
         }
     }
 
