bb49305e5a94113d98ea74706f9be90a6da1d4fe
Chris Sullens <cls.seals@gmail.com>
2016-11-22 16:29:04 -0500

    Jenkins automatic commit - 'jenkins-Screeps - Sync GitHub Repo-144'

--- tower.js
+++ tower.js
@@ -17,12 +17,12 @@ var tower = {
         var hostile = curTower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
         if (hostile) {
             curTower.attack(hostile)
-        } else {
-            // only repair if there are no enemies
-            var closestDamagedStructure = curTower.pos.findClosestByRange(FIND_STRUCTURES, { filter: (structure) => structure.hits < structure.hitsMax });
-            if (closestDamagedStructure) {
-                curTower.repair(closestDamagedStructure);
-            }
+        // } else {
+        //     // only repair if there are no enemies
+        //     var closestDamagedStructure = curTower.pos.findClosestByRange(FIND_STRUCTURES, { filter: (structure) => structure.hits < structure.hitsMax });
+        //     if (closestDamagedStructure) {
+        //         curTower.repair(closestDamagedStructure);
+        //     }
         }
     }
 };
