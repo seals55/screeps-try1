2c9498cec34a0492c99e3b3356c44b77b426d20c
Chris Sullens <cls.seals@gmail.com>
2016-11-21 21:01:04 -0500

    Jenkins automatic commit - 'jenkins-Screeps - Sync GitHub Repo-51'

--- vars.js
+++ vars.js
@@ -9,16 +9,8 @@ function define(name, value) {
 define("debug", true);
 
 // setup some minimum numbers for different roles
-define("minimumNumberOfHarvesters", 4);
-define("minimumNumberOfUpgraders", 2);
-define("minimumNumberOfBuilders", 2);
-define("minimumNumberOfRepairers", 1);
 
-define("minimumNumberOfTowerfillers", 1);
-define("maximumNumberOfTowerfillers", 1);
-
-define("minimumNumberOfStorers", 1);
-define("maximumNumberOfStorers", 1);
-
-define("minimumNumberOfWallRepairers", 0);
-define("maximumNumberOfWallRepairers", 1);
+define("maxMulti", 0);
+define("maxRepair", 2);
+define("maxHarvester", 4);
+define("maxUpgrader", 4);
