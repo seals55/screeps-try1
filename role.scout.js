ce3eaa27069a79b81b8a0d2e7f40da4a05c06a23
Chris Sullens <cls.seals@gmail.com>
2016-11-22 15:57:03 -0500

    Jenkins automatic commit - 'jenkins-Screeps - Sync GitHub Repo-126'

--- role.scout.js
+++ role.scout.js
@@ -8,38 +8,29 @@
  */
 
 var roleScout = {
-
     /** @param {Creep} creep **/
     run: function (creep) {
-        // var sources = creep.room.find(FIND_STRUCTURES, {
-        //     filter: (structure) => {
-        //         return (structure.structureType == STRUCTURE_CONTROLLER)
-        //     }
-        // });
-
-        // if (sources[0]) {
-        //     //move off page
-        //     //move DIR
-        //     creep.move(EAST)
-        // } else {
-        //     if (creep.claimController) == ERR_NOT_IN_RANGE) {
-        //         creep.moveTo(sources[0]);
-        //     }
-        // }
-
-        // if(creep.carry.energy == 0) {
-        //     var sources = creep.room.find(FIND_SOURCES);
-        //     if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-        //         creep.moveTo(sources[0]);
-        //     }
-        // }
-        // else {
-        //     if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-        //         creep.moveTo(creep.room.controller);
-        //     }
-        // }
+        moveToRoom = "W7N3"
+        //MOVE TO TARGET ROOM
+        if (creep.room != moveToRoom) {
+            var route = Game.map.findRoute(creep.room, moveToRoom);
+            if (route.length > 0) {
+                var exit = creep.pos.findClosestByRange(route[0].exit);
+                creep.moveTo(exit);
+            }
+        }
+        //RESERVE CONTROLLER IN TARGET ROOM (NOT WORKING, CREEP IS SWITCHING BETWEEN TARGET ROM AND THE ONE BEFORE THAT)
+        else {
+            if (creep.room.controller) {
+                if (creep.reserveController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(creep.room.controller);
+                }
+            }
+        }
     }
 };
 
 module.exports = roleScout;
+
+
 
