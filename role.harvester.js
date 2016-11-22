595677ef3b5ad619162b455b2d908e8d5fc499d6
Chris Sullens <cls.seals@gmail.com>
2016-11-21 20:33:02 -0500

    Jenkins automatic commit - 'jenkins-Screeps - Sync GitHub Repo-35'

--- role.harvester.js
+++ role.harvester.js
@@ -93,8 +93,11 @@ var roleMulti = {
             //////pos.findClosestByPath //creep.room.find
             var target = creep.pos.findClosestByRange(FIND_STRUCTURES, {
                 filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN || structure.structureType == STRUCTURE_CONTAINER || structure.structureType == STRUCTURE_TOWER) &&
-                        (structure.energy < structure.energyCapacity);
+                    return (structure.structureType == STRUCTURE_EXTENSION
+                        || structure.structureType == STRUCTURE_SPAWN
+                        || structure.structureType == STRUCTURE_CONTAINER
+                        || structure.structureType == STRUCTURE_TOWER)
+                        && (structure.energy < structure.energyCapacity);
                 }
             });
 
