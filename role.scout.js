c107ae1492b898ea6502726b76060d990ec08d08
Chris Sullens <cls.seals@gmail.com>
2016-11-22 15:46:03 -0500

    Jenkins automatic commit - 'jenkins-Screeps - Sync GitHub Repo-122'

--- role.scout.js
+++ role.scout.js
@@ -11,13 +11,16 @@ var roleScout = {
 
     /** @param {Creep} creep **/
     run: function (creep) {
-        var sources = creep.room.find(FIND_SOURCES);
+        var sources = creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType ==                     STRUCTURE_CONTROLLER));
+        
         if (sources[0]) {
             //move off page
             //move DIR
             creep.move(EAST)
         } else {
-            if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+            if (creep.claimController) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0]);
             }
         }
