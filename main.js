d7267aaf8f8b00c33da4585b0d9dd35bca8262e9
Chris Sullens <cls.seals@gmail.com>
2016-11-25 20:53:03 -0500

    Jenkins automatic commit - 'jenkins-Screeps - Sync GitHub Repo-256'

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ module.exports.loop = function () {
         var spawn = curRoom.find(FIND_STRUCTURES, {filter: { structureType: STRUCTURE_SPAWN } })
         //
         //
-        //helper.buildRoads(curRoom);
+        helper.buildRoads(curRoom);
         //helper.removeUnfinishedRoads(curRoom);
         //
         //        
