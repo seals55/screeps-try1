a3940120151b9c3445d0ad1e3967a27563a67bce
Chris Sullens <cls.seals@gmail.com>
2016-11-25 21:11:02 -0500

    Jenkins automatic commit - 'jenkins-Screeps - Sync GitHub Repo-264'

--- role.harvester.js
+++ role.harvester.js
@@ -4,14 +4,36 @@ var roleMulti = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        if (creep.carry.energy == 0) {
+        /*var exits = Game.map.describeExits(creep.room.name)
+        var totExits = 4;
+        console.log('Exits: ' + totExits)
+        for (var x in exits){
+            for (var r in Game.rooms) {
+                if (Game.rooms[r].name == exits[x]) {
+                    delete exits[x];
+                    totExits -= 1;
+                    break;
+                }
+            }
+        }
+        console.log(creep.room.name + ' Exits: ' + totExits)*/
+        
+        /*for (var i = 0; i < 50; i++) {
+            var ter = Game.map.getTerrainAt(0,i,creep.room.name);
+            if (ter == plain || ter == swamp) {
+                
+            }
+        }*/
+        
+        
+        if(creep.carry.energy == 0){
             creep.memory.working = false;
         }
-        if (creep.carry.energy < creep.carryCapacity && creep.memory.working == false) {
+        if(creep.carry.energy < creep.carryCapacity && creep.memory.working == false) {
             //init vars
             var sources = creep.room.find(FIND_SOURCES);
             var creepCount = new Array()
-
+            
             /////////////////////////////////////////////////////////////////////////////////////////////////////////
             //Has harvesting source been determined? Check
             if (creep.memory.harSource = -1) {
@@ -23,7 +45,7 @@ var roleMulti = {
                     }
                 }
                 for (i = 0; i < sources.length; i++) {
-                    for (var cName in Game.creeps) {
+                    for(var cName in Game.creeps) {
                         var cre = Game.creeps[cName];
                         if (cre.memory.harSource != -1) {
                             creepCount[cre.memory.harSource] += 1;
@@ -31,7 +53,7 @@ var roleMulti = {
                         }
                     }
                 }
-
+                
                 //console.log('CreepCount[0]: ' + creepCount[0] + ' CreepCount[1]: ' + creepCount[1])
                 /////////////////////////////////////////////////////////////////////////////////////////////////////////
                 //Determine best source to send creep to
@@ -44,8 +66,9 @@ var roleMulti = {
                     }
                 }
                 //console.log('LowestCreepSource: ' +lowestCreepSource)
-                creep.memory.harSource = lowestCreepSource;
-
+                if(creep.memory.harSource == -1 || sources[creep.memory.harSource].energy==0){
+                    creep.memory.harSource = lowestCreepSource;
+                }
                 var sourceFound = false;
                 for (i = 0; i < sources.length; i++) {
                     if (sources[i].energy > 0) {
@@ -54,25 +77,23 @@ var roleMulti = {
                     }
                 }
             }
-
+            
             //check for dropped energy
-            var dropenergy = creep.pos.findClosestByPath(FIND_DROPPED_ENERGY, { filter: (d) => { return (d.resourceType == RESOURCE_ENERGY) } });
+            var dropenergy = creep.pos.findClosestByPath(FIND_DROPPED_ENERGY, { filter: (d) => {return (d.resourceType == RESOURCE_ENERGY)} });
             if (dropenergy) {
                 if (creep.pickup(dropenergy) == ERR_NOT_IN_RANGE) {
-                    //helper.routeCreep(creep, dropenergy);
                     creep.moveTo(dropenergy);
                 }
             } else {
                 /////////////////////////////////////////////////////////////////////////////////////////////////////////
                 //Send creep to source
-                if (sourceFound == true) {
-                    if (creep.harvest(sources[creep.memory.harSource]) == ERR_NOT_IN_RANGE) {
-                        //helper.routeCreep(creep, sources[creep.memory.harSource]);
+                if(sourceFound == true) {
+                    if(creep.harvest(sources[creep.memory.harSource]) == ERR_NOT_IN_RANGE) {
                         creep.moveTo(sources[creep.memory.harSource]);
                     }
                 } else if (creep.carry.energy == 0) {
                     //move to bored flag
-                    console.log(creep.room.name + ' Harvester moving to bored')
+                    //console.log(creep.room.name + ' Harvester moving to bored')
                     for (var flag in Game.flags){
                         if (Game.flags[flag].pos.roomName == creep.room.name && creep.room.name.substring(0,5) == 'Bored') {
                             creep.moveTo(Game.flags[flag]);
@@ -103,17 +124,15 @@ var roleMulti = {
             });
             // if (vars.debug) { console.log("1. Harvester target: " + target) };
             if (!target) {
-                var target = creep.pos.findClosestByRange(FIND_STRUCTURES, {                    filter: (structure) => 
-		{
-                        return (structure.structureType == STRUCTURE_EXTENSION
-                            || structure.structureType == STRUCTURE_SPAWN
-                            || structure.structureType == STRUCTURE_CONTAINER)
-                            && (structure.energy < structure.energyCapacity);
-                    }
-                });
+            var target = creep.pos.findClosestByRange(FIND_STRUCTURES, { filter: (structure) => 
+                                                                {
+                                                                    return ( structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN || structure.structureType == STRUCTURE_CONTAINER || structure.structureType == STRUCTURE_TOWER ) &&
+                                                                    (structure.energy < structure.energyCapacity);
+                                                                }
+                                                            });
                 // if (vars.debug) { console.log("2. Harvester target: " + target) };
             }
-
+                                                            
             if (!target) {
                 var target = creep.pos.findClosestByRange(FIND_STRUCTURES, {
                     filter: (structure) => {
@@ -126,15 +145,14 @@ var roleMulti = {
             }
 
 
-            //structure.store[RESOURCE_ENERGY] < structure.storeCapacity
-            if (target == null) {
+                                                            //structure.store[RESOURCE_ENERGY] < structure.storeCapacity
+            if(target == null) {
                 target = creep.pos.findClosestByRange(FIND_STRUCTURES, { filter: (structure) => { return structure.structureType == STRUCTURE_STORAGE && _.sum(structure.store) < structure.storeCapacity; } });
             }
-            if (target != null) {
+            if(target != null) {
                 /////////////////////////////////////////////////////////////////////////////////////////////////////////
                 //Structures found, deposit energy
-                if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    //helper.routeCreep(creep, target);
+                if(creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(target);
                 }
             }
